3232867a50de64c3f114b2d1d5aec540edca7b40
jjunik <tjwnsdlr4545@gmail.com>
2024-05-13 21:46:32 +0900

    Add files via upload

---
+++ Day02/TypeCasting.java
@@ -0,0 +1,17 @@
+package Day02;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        System.out.println(a*b);
+
+        String c = "30";
+        String d = "50";
+        System.out.println(a+c);
+        System.out.println(c+d);
+
+
+
+    }
+}
